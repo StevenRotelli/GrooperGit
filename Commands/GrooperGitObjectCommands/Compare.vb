76019a206a455659476fcccfede6c82cca3ca920
STEVEN ROTELLI <strotelli@bisok.com>
2023-09-25 12:26:53 -0500

    Repair

---
+++ Commands/GrooperGitObjectCommands/Compare.vb
@@ -0,0 +1,36 @@
+''' <summary>Executes a Git Compare/Diff operation, showing differences between files or branches in the specified GitProject [Object Command].</summary>
+''' <remarks>A Git Compare/Diff highlights the changes between two sets of code, aiding in understanding modifications and aiding in code review. More details can be found <a href="https://git-scm.com/docs/git-diff">here</a>.</remarks>
+<DataContract, IconResource("Diff"), DisplayName("Compare")>
+Public Class Compare : Inherits ObjectCommand(Of GrooperNode)
+    ''' <summary>Code that will be executed when the [Object Command] is executed.</summary>
+    ''' <param name="Item">The Grooper object that the command will execute against.</param>
+    Protected Overrides Sub Execute(Item As GrooperNode)
+
+    End Sub
+    Protected Overrides Function CanExecute(Item As GrooperNode) As Boolean
+        Dim myTypes As List(Of Type) = New List(Of Type) From {
+        GetType(Project), GetType(GrooperRoot), GetType(MachinesFolder), GetType(ThreadPool),
+        GetType(Machine), GetType(BatchObject), GetType(BatchesFolder), GetType(Grooper.ScriptObject), GetType(GrooperRoot)
+    }
+
+        If Not myTypes.Contains(Item.GetType()) AndAlso Not myTypes.Contains(Item.GetType().BaseType) Then
+            'MsgBox(Item.GetType().BaseType.FullName & " " & Item.Name)
+            If TypeOf Item.ParentProject Is GitProject Then
+                Return True
+            End If
+        End If
+
+        Return False ' Add a return statement in case none of the conditions are met.
+    End Function
+
+    Protected Overrides Function IsPropertyVisible(PropertyName As String) As Boolean?
+        Return MyBase.IsPropertyVisible(PropertyName)
+    End Function
+    Protected Overridable Function Types(ByVal t As Type()) As List(Of String)
+        Dim innerList As New List(Of String)
+        For Each i In t
+            innerList.Add(i.Name)
+        Next
+        Return innerList
+    End Function
+End Class
---
+++ Commands/GrooperGitObjectCommands/Stage.vb
@@ -0,0 +1,39 @@
+''' <summary>Executes a Git Stage operation, preparing changes for commit in the specified GitProject [Object Command].</summary>
+''' <remarks>Git Staging allows you to prepare changes for a commit, helping you organize modifications and facilitating code review. Learn more about Git Staging <a href="https://git-scm.com/docs/git-stage">here</a>.</remarks>
+<DataContract, IconResource("Stage"), DisplayName("Stage")>
+Public Class Stage : Inherits ObjectCommand(Of GrooperNode)
+    ''' <summary>Code that will be executed when the [Object Command] is executed.</summary>
+    ''' <param name="Item">The Grooper object that the command will execute against.</param>
+    Protected Overrides Sub Execute(Item As GrooperNode)
+        Dim Project As GitProject.GitProject_Properties = Item.ParentProject.NodeProps
+        Project.ChangedIds.Add(Item.Id)
+    End Sub
+    Protected Overrides Function CanExecute(Item As GrooperNode) As Boolean
+        Dim myTypes As List(Of Type) = New List(Of Type) From {
+        GetType(Project), GetType(GrooperRoot), GetType(MachinesFolder), GetType(ThreadPool),
+        GetType(Machine), GetType(BatchObject), GetType(BatchesFolder), GetType(Grooper.ScriptObject), GetType(GrooperRoot)
+    }
+
+        If Not myTypes.Contains(Item.GetType()) AndAlso Not myTypes.Contains(Item.GetType().BaseType) Then
+            If TypeOf Item.ParentProject Is GitProject Then
+                Dim Parent As GitProject = Item.ParentProject
+                If Parent.ChangedNodes.Contains(Item) = False And Item.HasValue("GitStatus") Then
+                    Return True
+                End If
+            End If
+        End If
+
+        Return False
+    End Function
+
+    Protected Overrides Function IsPropertyVisible(PropertyName As String) As Boolean?
+        Return MyBase.IsPropertyVisible(PropertyName)
+    End Function
+    Protected Overridable Function Types(ByVal t As Type()) As List(Of String)
+        Dim innerList As New List(Of String)
+        For Each i In t
+            innerList.Add(i.Name)
+        Next
+        Return innerList
+    End Function
+End Class
